2d9b2ddb626eb15e5b6a77fffcb62e1a6d50efef
shinya takahashi <s.takahashi313@gmail.com>
2014-06-23 21:34:43 +0900

    replace brfs to partialify

--- public/all.js
+++ public/all.js
@@ -1,4 +1,9 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var fs;Vue.component("hoge",{template:"<div>\n  Hello World\n</div>\n"}),$(function(){var e;return e=new Vue({el:"#app",data:{currentView:"hoge"}})});
+Vue.component("hoge",{template:require("./views/hoge.html")}),$(function(){var e;return e=new Vue({el:"#app",data:{currentView:"hoge"}})});
+//# sourceMappingURL=out.js.map
+
+},{"./views/hoge.html":2}],2:[function(require,module,exports){
+module.exports="<div>\n  Hello World\n</div>\n";
+//# sourceMappingURL=out.js.map
 
 },{}]},{},[1])
